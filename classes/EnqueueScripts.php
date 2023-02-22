c842d8b7ce85f116cad5dedabb5020dbe672d59d
Ibrarr <ibrarr2000@gmail.com>
2023-02-22 11:22:35 +0000

    Version scripts

--- classes/EnqueueScripts.php
+++ classes/EnqueueScripts.php
@@ -12,10 +12,13 @@ class EnqueueScripts
     public function enqueue_scripts()
     {
         if (is_page_template('templates/scorecard.php') || is_page_template('templates/results.php')) {
-            wp_register_style('scorecard-style', plugin_dir_url(dirname(__FILE__)) . 'dist/css/app.css', [], 1, 'all');
+            $my_css_ver  = date("ymd-Gis", filemtime(plugin_dir_path(dirname(__FILE__)) . '/dist/css/app.css'));
+            $my_js_ver = date("ymd-Gis", filemtime(plugin_dir_path(dirname(__FILE__)) . 'dist/js/app.js'));
+
+            wp_register_style('scorecard-style', plugin_dir_url(dirname(__FILE__)) . 'dist/css/app.css', [], $my_css_ver, 'all');
             wp_enqueue_style('scorecard-style');
 
-            wp_register_script('scorecard-script', plugin_dir_url(dirname(__FILE__)) . 'dist/js/app.js', ['jquery'], 1, true);
+            wp_register_script('scorecard-script', plugin_dir_url(dirname(__FILE__)) . 'dist/js/app.js', ['jquery'], $my_js_ver, true);
             wp_enqueue_script('scorecard-script');
         }
     }
