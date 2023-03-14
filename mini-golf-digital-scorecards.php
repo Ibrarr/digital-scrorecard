7efe3d11f209c49136813436633be0ce3cbd8b56
Ibrarr <ibrarr2000@gmail.com>
2023-03-14 23:26:54 +0000

    headers

--- mini-golf-digital-scorecards.php
+++ mini-golf-digital-scorecards.php
@@ -1,12 +1,23 @@
 <?php
 
 /**
- * Plugin Name: Mini-Golf Digital Scorecard
- * Description: Creates the templates necessary for the digital scorecards
- * Version: 1.0.0
- * Author: Ibrarr Khan
- * License: GPLv2 or later
- * Text Domain: digital-scorecard
+ * Mini-Golf Digital Scorecard
+ *
+ * @package           DigitalScorecard
+ * @author            Ibrarr Khan
+ * @copyright         2023 Ibrarr Khan
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Mini-Golf Digital Scorecard
+ * Plugin URI:        https://github.com/Ibrarr/mini-golf-digital-scorecards
+ * Description:       Creates post templates for your digital scorecard
+ * Version:           1.0.0
+ * Author:            Ibrarr Khan
+ * Author URI:        http://ibrarrkhan.com/
+ * Text Domain:       digital-scorecard
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
